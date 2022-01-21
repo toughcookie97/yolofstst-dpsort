1ac190a9e1460971227656d59ca27b516ebd4cdd
Mycandy <41058740+toughcookie97@users.noreply.github.com>
2022-01-21 11:14:44 +0800

    Add files via upload

---
+++ image_yolov3.sh
@@ -0,0 +1,4 @@
+./darknet detector test /home/limzero/xjy/yfst/build/darknet/x64/data/voc.data ./yolo-fastest-1.1-640.cfg ./yolo-fastest-1-640.weights data/0069.bmp -i 0 -thresh 0.25
+
+
+
---
+++ train_yolofastest.sh
@@ -0,0 +1,4 @@
+./darknet detector train /home/limzero/xjy/yfst/build/darknet/x64/data/voc.data ./yolo-fastest-1.1-640.cfg /home/limzero/xjy/yfst/build/darknet/x64/pretrained_model/yolo-fastest.conv.109
+
+
+
